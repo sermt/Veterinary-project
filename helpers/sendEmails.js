2484184570f9cfeb00ef8478e92a35073741fa45
sermt <sermi.cima@gmail.com>
2022-03-15 00:00:38 -0600

    first commi222

--- helpers/sendEmails.js
+++ helpers/sendEmails.js
@@ -1,11 +1,11 @@
 import nodemailer from "nodemailer";
 const emailRegistry = async ({ name, email, token }) => {
   var transport = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST.toString(),
-    port: process.env.EMAIL_PORT.toString(),
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
-      user: process.env.EMAIL_USER.toString(),
-      pass: process.env.EMAIL_PASSWORD.toString(),
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
