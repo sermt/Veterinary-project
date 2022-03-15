eef2a0d4c6149a226309c9f77bece73312e3d37b
sermt <sermi.cima@gmail.com>
2022-03-15 02:17:44 -0600

    first1 commits3

--- helpers/recoveryPassword.js
+++ helpers/recoveryPassword.js
@@ -9,7 +9,7 @@ const recoveryPassword = async ({ name, email, token }) => {
 });
 
   try {
-    const info = await transport.sendMail({
+     await transport.sendMail({
       from: "Vet App",
       to: email,
       subject: "Recovery Password",
@@ -17,9 +17,17 @@ const recoveryPassword = async ({ name, email, token }) => {
       html: `<h1>Hello ${name}!</h1><br /><p>follow the link below to recover your password,
         <a href="http://localhost:3000/forget-Password/${token}">Click here</a></p><br />
           <p>if you have not requested the recovery please ignore this message</p>`,
+    },function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
     });
     
-  } catch (error) {}
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default recoveryPassword;
