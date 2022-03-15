52fb1640b90c9acaad996b053320b953d9f08d90
sermt <sermi.cima@gmail.com>
2022-03-15 00:12:35 -0600

    first commit3333

--- helpers/sendEmails.js
+++ helpers/sendEmails.js
@@ -1,13 +1,12 @@
 import nodemailer from "nodemailer";
 const emailRegistry = async ({ name, email, token }) => {
-  var transport = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+  var transport = nodemailer.createTransport("SMTP", {
+    service: "hotmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+    }
+});
 
   try {
     const info = await transport.sendMail({
