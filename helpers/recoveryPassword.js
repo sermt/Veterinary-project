468229183ca8efb77bf5ffd5708a94202807ad13
sermt <sermi.cima@gmail.com>
2022-03-15 03:19:54 -0600

    first1 commits3

--- helpers/recoveryPassword.js
+++ helpers/recoveryPassword.js
@@ -4,7 +4,7 @@ const recoveryPassword = async ({ name, email, token }) => {
   const transfer = nodemailer.createTransport({
     service: "gmail",
     port: 587,
-    secure: false,
+    secure: true,
     auth:{ 
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
