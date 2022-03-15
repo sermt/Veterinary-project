bbc2a619b33c56ab40134858cedea24a73914e7e
sermt <sermi.cima@gmail.com>
2022-03-15 03:12:34 -0600

    first1 commits3

--- helpers/recoveryPassword.js
+++ helpers/recoveryPassword.js
@@ -1,33 +1,31 @@
-import nodemailer from "nodemailer";
+import mailjet  from 'node-mailjet'
 const recoveryPassword = async ({ name, email, token }) => {
-  const transport = nodemailer.createTransport({
-    service: "hotmail",
-    auth: {
+
+  const transfer = nodemailer.createTransport({
+    service: "gmail",
+    port: 587,
+    secure: false,
+    auth:{ 
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
     }
 });
 
-  try {
-     await transport.sendMail({
-      from: "Vet App",
-      to: email,
-      subject: "Recovery Password",
-      text: "Recovery Password",
-      html: `<h1>Hello ${name}!</h1><br /><p>follow the link below to recover your password,
-        <a href="http://localhost:3000/forget-Password/${token}">Click here</a></p><br />
-          <p>if you have not requested the recovery please ignore this message</p>`,
-    },function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
-    
-  } catch (error) {
-    console.error(error);
-  }
+let mailBilgi = {
+    from:  process.env.EMAIL_USER,
+    to: email,
+    subject: "Recovery Password",
+    html:`<h1>Hello ${name}!</h1><br /><p>follow the link below to recover your password,
+    <a href="http://localhost:3000/forget-Password/${token}">Click here</a></p><br />
+      <p>if you have not requested the recovery please ignore this message</p>`,
+};
+
+transfer.sendMail(mailBilgi, err => {
+    if(err){
+        next(err);
+    }
+});
+
 };
 
 export default recoveryPassword;
