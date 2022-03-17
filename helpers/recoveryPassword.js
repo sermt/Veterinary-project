be37c790ceec84732058046534a9e431b7e0a2e6
sermt <sermi.cima@gmail.com>
2022-03-17 16:45:24 -0600

    first1 commits31111

--- helpers/recoveryPassword.js
+++ helpers/recoveryPassword.js
@@ -1,31 +1,30 @@
 import nodemailer from "nodemailer";
+ // send an email to the user who has requested a new password
 const recoveryPassword = async ({ name, email, token }) => {
-
   const transfer = nodemailer.createTransport({
     service: "gmail",
     port: 587,
     secure: false,
-    auth:{ 
+    auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
-    }
-});
+    },
+  });
 
-let mailBilgi = {
-    from:  process.env.EMAIL_USER,
+  let mailBilgi = {
+    from: process.env.EMAIL_USER,
     to: email,
     subject: "Recovery Password",
-    html:`<h1>Hello ${name}!</h1><br /><p>follow the link below to recover your password,
-    <a href="https://confident-hawking-834487.netlify.app/forget-Password/${token}">Click here</a></p><br />
-      <p>if you have not requested the recovery please ignore this message</p>`,
-};
+    html: `<h1>Hello ${name}!</h1><br /><p> follow the link below to recover your password,
+    <a href="https://confident-hawking-834487.netlify.app/forget-Password/${token}"> Click here.</a></p><br />
+      <p>If you have not requested the recovery please ignore this message.</p>`,
+  };
 
-transfer.sendMail(mailBilgi, err => {
-    if(err){
-        console.log(err);
+  transfer.sendMail(mailBilgi, (err) => {
+    if (err) {
+      console.log(err);
     }
-});
-
+  });
 };
 
 export default recoveryPassword;
--- helpers/sendEmails.js
+++ helpers/sendEmails.js
@@ -1,31 +1,35 @@
 import nodemailer from "nodemailer";
+// sending an email to verify
 const emailRegistry = async ({ name, email, token }) => {
   const transfer = nodemailer.createTransport({
     service: "gmail",
     port: 587,
     secure: false,
-    auth:{ 
+    auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
-    }
-});
+    },
+  });
 
   try {
-     await transfer.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "Account verification",
-      text: "Please verify your account",
-      html: `<h1>Hello ${name}!</h1><br /><p>Please verify your account,
-        <a href="https://confident-hawking-834487.netlify.app/confirm/${token}">Clic here to verify</a></p><br />
-          <p>if you don't create a new account, please ignore this message</p>`,
-    },function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }});
-   
+    await transfer.sendMail(
+      {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "Account verification",
+        text: "Please verify your account",
+        html: `<h1>Hello ${name}!</h1><br /><p>Please verify your account,
+        <a href="https://confident-hawking-834487.netlify.app/confirm/${token}"> Click here to verify.</a></p><br />
+          <p>If you don't create a new account, please ignore this message.</p>`,
+      },
+      function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      }
+    );
   } catch (error) {
     console.log(error);
   }
